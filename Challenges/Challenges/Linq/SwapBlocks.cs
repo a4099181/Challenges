7441f121b714440203459b7b6e992a215ad4982f
seb! <seb!>
2016-03-05 11:35:44 +0100

    SwapBlocks with Linq.

---
+++ Challenges/Challenges/Linq/SwapBlocks.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Challenges.Challenges.Linq
+{
+    class SwapBlocks
+    {
+        internal static string Swap( string input, int n )
+            => new string( Swap( input.ToCharArray(), n ) );
+
+        static char[] Swap( char[] array, int n )
+            => array.Skip( n ).Concat( array.Take( n ) ).ToArray();
+    }
+}
---
+++ Challenges/Tests/Linq/SwapBlocksSpec.cs
@@ -0,0 +1,24 @@
+﻿using Challenges.Challenges.Linq;
+using Machine.Specifications;
+
+namespace Challenges.Tests.Linq
+{
+    [Subject( typeof ( SwapBlocks ) )]
+    class SwapBlocksSpec
+    {
+        Because _of = () =>
+        {
+            _foobar = SwapBlocks.Swap( "foobar", 3 );
+            _jamesBond = SwapBlocks.Swap( "JamesBond", 5 );
+        };
+
+        It _swappedFoobarShouldBeBarfoo =
+            () => _foobar.ShouldEqual( "barfoo" );
+
+        It _swappedJamesBondShouldBeBondJames =
+            () => _jamesBond.ShouldEqual( "BondJames" );
+
+        static string _foobar;
+        static string _jamesBond;
+    }
+}
