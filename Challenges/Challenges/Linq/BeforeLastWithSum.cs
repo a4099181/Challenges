c40385f525f2bafbd5b041791f394b5f0a2a8ec8
seb! <seb!>
2016-03-05 11:35:55 +0100

    BeforeLast with Linq/Sum.

---
+++ Challenges/Challenges/Linq/BeforeLastWithSum.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenges.Challenges.Linq
+{
+    class BeforeLastWithSum
+    {
+        internal static char Get( IList<char> input )
+            => input[ GetIndex( input ) ];
+
+        internal static int GetIndex( IList<char> input )
+            => input.Select( _ => 1 ).Sum() - 2;
+    }
+}
